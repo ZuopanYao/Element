6b929b67f524da976459592e2d4d7c28f9f48788
ZuopanYao <yaozuopan@icloud.com>
2019-05-04 16:50:40 +0800

    fix bug

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Element",
 	dependencies: [
-        .package(url: "https://github.com/ZuopanYao/swift-utils.git", from: "1.0.0")
-    ],
-	exclude: ["README.md"]
-) 
+        .package(url: "https://github.com/ZuopanYao/swift-utils.git", from: "1.0.2")
+    ]
+)
